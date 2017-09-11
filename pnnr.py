02b8b5053fe36ade296f9ab56fb14c99ea46c2f9
barlaensdoonn <brandon.aleson@gmail.com>
2017-09-10 18:53:58 -0700

    refactor into a class

--- pnnr.py
+++ pnnr.py
@@ -9,59 +9,60 @@ from pygame import mixer
 from pygame import time as pytime
 
 
-prefix = '/home/pi/pal_combo/ignore/sound/alex'
-files = [
-    'alex_NE_100_44.wav',
-    'alex_VER_100_44.wav',
-    'alex_ODD_100_44.wav',
-    'alex_OR_100_44.wav',
-    'alex_EV_100_44.wav',
-    'alex_EN_100_44.wav'
-]
+class Panner(object):
 
-filepaths = [os.path.join(prefix, files[i]) for i in range(len(files))]
-sounds = [mixer.Sound(filepath) for filepath in filepaths]
-left = (1, 0)
-right = (0, 1)
-mixer.init(frequency=44100, buffer=1024)  # default buffer is 4096
+    search_path = '/home/pi/pal_combo/ignore/sound/alex'
 
+    def __init__(self, soundpath=search_path):
+        self.pan_left = (1, 0)
+        self.pan_right = (0, 1)
+        self.mixer = mixer
+        self.mixer.init(frequency=44100, buffer=1024)  # default buffer is 4096
+        self._init_sounds(soundpath)
+        self.waits = self._calculate_waits()
 
-def wait(channel):
-    '''
-    pause until channel is done playing sound. using this technique results
-    in noticeable gaps between the end of one sound and start of next.
-    calculate_waits technique takes this into account
-    '''
-    while channel.get_busy():
-        pass
+    def _init_sounds(self, soundpath):
+        filepaths = []
 
+        for dirpath, dirnames, filenames in os.walk(soundpath):
+            for thing in filenames:
+                filepaths.append(os.path.join(dirpath, thing))
 
-def calculate_waits(sounds):
-    '''return list of wait times in ms calculated as length of sound - 100ms'''
+        self.sounds = [self.mixer.Sound(filepath) for filepath in filepaths]
 
-    lngths = []
-    for i in range(len(sounds)):
-        lngth = sounds[i].get_length()
-        lngth = lngth * 1000 - 100
-        lngths.append(int(lngth))
+    def _wait(self, channel):
+        '''
+        pause until channel is done playing sound. using this technique results
+        in noticeable gaps between the end of one sound and start of next.
+        calculate_waits technique takes this into account
+        '''
+        while channel.get_busy():
+            pass
 
-    return lngths
+    def _calculate_waits(self):
+        '''return list of wait times in ms calculated as length of sound - 100ms'''
 
+        lngths = []
+        for i in range(len(self.sounds)):
+            lngth = self.sounds[i].get_length()
+            lngth = lngth * 1000 - 100
+            lngths.append(int(lngth))
 
-def loop_by_length(sounds):
-    waits = calculate_waits(sounds)
-    clock = pytime.Clock()
+        return lngths
 
-    for i in range(len(sounds)):
-        clock.tick()
-        channel = mixer.find_channel()
+    def loop_once(self):
+        clock = pytime.Clock()
 
-        if not i % 2:
-            channel.set_volume(*left)
-        else:
-            channel.set_volume(*right)
+        for i in range(len(self.sounds)):
+            clock.tick()
+            channel = self.mixer.find_channel()
 
-        channel.play(sounds[i])
-        pytime.wait(waits[i])
-        # channel.stop()
-        print(clock.get_time())
+            if not i % 2:
+                channel.set_volume(*self.pan_left)
+            else:
+                channel.set_volume(*self.pan_right)
+
+            channel.play(self.sounds[i])
+            pytime.wait(self.waits[i])
+            # channel.stop()
+            print(clock.get_time())
